590ea9ae5fa4c29530b4485cc525eeab6873bf78
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-22 00:54:12 +0000

    complete working

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const array1 = [1982, 1985, 1986, 1990, 1970, 2005, 2001, 2000];
-const array2 = [];
+const year1 = [1977, 2010, 1988, 2011, 2001, 1999];
+const year2 = [2010, 1989, 2011, 1976, 1982, 2000];
 
+const age1 = [];
+const age2 = [];
+
+booleanArray1 = [];
+booleanArray2 = [];
+
+// function calculates age
 function calculateAge(yearOfBirth) {
-  return new Date().getFullYear() - yearOfBirth;
+  let age = new Date().getFullYear() - yearOfBirth;
+  return age;
 }
 
-//Push age to array2
-for (let i of array1) {
-  array2.push(calculateAge(i));
-}
-
-// Logs age to console
-for (let i of array2) {
-  if (i >= 18) {
-    console.log(i, "is of full age");
-  } else {
-    console.log(i, "is undr age");
-  }
-}
-
-let arrayA = [];
-
-function printFullAge(year) {
-  let age = calculateAge(year);
+//Function checs age and logs to console
+function checkIf18(age) {
   if (age >= 18) {
-    arrayA.push(true);
+    console.log(age, "is of age");
+    return true;
   } else {
-    arrayA.push(false);
+    console.log(age, "is under-age");
+    return false;
   }
-  return arrayA;
 }
 
-for (let i of array1) {
-  printFullAge(i);
+// Function takes input and output arrays as parameters and returns  boolean values array
+function returnArrayOfBooleans(yearArray, booleanArray) {
+  for (let i of yearArray) {
+    let age = calculateAge(i);
+    let if18 = checkIf18(age);
+    booleanArray.push(if18);
+  }
+  return booleanArray;
 }
+
+returnArrayOfBooleans(year1, booleanArray1);
+returnArrayOfBooleans(year2, booleanArray2);
